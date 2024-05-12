c6f5c85f86d927df5a24beb0b2d7b1a44a23e20f
aerinp <s5253812@gmail.com>
2024-05-11 21:20:34 -0700

    Add Highest Score

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,8 @@
 from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = ('Arial', 20, "bold")
+TEXT_PATH = "highest_score.txt"
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
@@ -7,20 +10,41 @@ class Scoreboard(Turtle):
         self.penup()
         self.hideturtle()
         self.color("white")
+        self.highest = 0
+        self.retrieve_highest()
         self.update_score()
-    
+
     def score_up(self):
         self.score += 1
+        if self.score > self.highest:
+            self.highest = self.score
+            self.update_highest()
         self.update_score()
+
+    def retrieve_highest(self):
+        with open(TEXT_PATH, mode="a+") as file:
+            file.seek(0)
+            content = file.read()
+            if content != "":
+                self.highest = int(content)
+                print(content)
+            file.close()
+        
+    def update_highest(self):
+        with open(TEXT_PATH, mode="w") as file:
+            file.write(f"{self.highest}")
+            file.close()
 
     def update_score(self):
         self.clear()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", True, align="center", font=('Arial', 20, "bold"))
+        self.write(f"Score: {self.score}\t Highest: {self.highest}", True, align=ALIGNMENT, font=FONT)
 
     def game_over(self):
         self.clear()
         self.goto(0, 0)
-        self.write("GAME OVER! :()", True, align="center", font=('Arial', 20, "bold"))
+        self.write("GAME OVER! :()", True, align=ALIGNMENT, font=FONT)
         self.goto(0, -30)
-        self.write(f"Final Score: {self.score}", True, align="center", font=('Arial', 20, "bold"))
+        self.write(f"Final Score: {self.score}", True, align=ALIGNMENT, font=FONT)
+        self.goto(0, -60)
+        self.write(f"Highest Score: {self.highest}", True, align=ALIGNMENT, font=FONT)
